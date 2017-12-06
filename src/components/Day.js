c1514445c0689684166c1b03d84e97f059393d0f
Andrea Cardy <acardy@customink.com>
2017-12-06 06:51:54 -0500

    Displaying State with JSX - Final

--- src/components/Day.js
+++ src/components/Day.js
@@ -1,16 +1,32 @@
 import React from 'react';
 
 class Day extends React.Component {
+  formatDate(datetime) {
+    let thisdate = new Date(datetime);
+    return thisdate.toLocaleDateString('en-US',
+    { 
+      month: 'short',
+      day: 'numeric'
+    });
+  }
+  getDayOfWeek(datetime) {
+    let thisdate = new Date(datetime);
+    return thisdate.toLocaleDateString('en-US',
+    {
+     weekday: 'short'
+    });
+  }
   render() {
+    const day = this.props.day;
     return (
       <div className="day">
-        <h2>TODAY</h2>
-        <h3>Nov 24</h3>
-        <img src={process.env.PUBLIC_URL + "/weather_icons/01d.png"} alt="weather" />
+        <h2>{ this.getDayOfWeek(day.dt) }</h2>
+        <h3>{ this.formatDate(day.dt) }</h3>
+        <img src={ process.env.PUBLIC_URL + "/weather_icons/" + day.icon + ".png" } alt="weather" />
         <div className="degrees">
-          <span className="high">81&deg;</span>/64&deg;
+          <span className="high">{ day.temp_max }&deg;</span>/{ day.temp_min }&deg;
         </div>
-        <p>A p.m. shower or thunderstorm.</p>
+        <p>{ day.description }</p>
       </div>
     )
   }
--- src/components/WeatherApp.js
+++ src/components/WeatherApp.js
@@ -2,12 +2,28 @@ import React from 'react';
 import '../css/weather-app.css';
 import Days from './Days';
 import DetailedForecast from './DetailedForecast';
+import sampleCity from '../sample-city';
 
 class WeatherApp extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      city: [],
+      currentDay:{}
+    }
+  }
+  componentWillMount() {
+    this.setState({
+      city: sampleCity
+    });
+  }
   render() {
     return (
       <main>
-        <Days cityState={this.props.match.params.weatherId} />
+        <Days
+          cityState={ this.props.match.params.weatherId }
+          data={ this.state }
+        />
         <DetailedForecast />
       </main>
     )
